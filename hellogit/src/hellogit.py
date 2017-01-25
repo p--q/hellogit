3ae7f9224568050014166291de51ba45f0e31fa2
pq <pq.blogspot@gmail.com>
2017-01-25 18:07:54 +0900

    added a print out and two new characters

--- hellogit/src/hellogit.py
+++ hellogit/src/hellogit.py
@@ -1,5 +1,6 @@
-names = ["Bilbo","Frodo","Aragorn","Leglas","Gandalf"]
+names = ["Bilbo","Frodo","Aragorn","Leglas","Gandalf","Boromir","Faramir"]
 # print greetings
 for i in range(0,len(names)):
     print("Hello, {}".format(names[i]))
+    print("How are you doing today?")
     
