861da928c94c0cda88a5d0350b53c0fbbc2dd327
pq <pq.blogspot@gmail.com>
2017-01-25 17:58:52 +0900

    converted for loop

--- hellogit/src/hellogit.py
+++ hellogit/src/hellogit.py
@@ -1,5 +1,5 @@
 names = ["Bilbo","Frodo","Aragorn","Leglas","Gandalf"]
-# print greetings
-for i in range(0,len(names)):
-    print("Hello, {}".format(names[i]))
+# print greetings to the fine folks in the Middle Earth
+for name in names:
+    print("Hello, {}".format(name))
     
